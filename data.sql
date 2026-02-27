ce0469e15d9e4d010407f92c66c6302d823f9a84
Ranjith.HL <ranjithranjith2747@gmail.com>
2026-02-27 23:10:52 +0530

    Create data.sql

---
+++ data.sql
@@ -0,0 +1,4 @@
+INSERT INTO students (name, roll_number, subject1, subject2, subject3, total, percentage, grade)
+VALUES
+('Ravi Kumar', '101', 85, 90, 88, 263, 87.67, 'A'),
+('Sneha Reddy', '102', 75, 70, 80, 225, 75.00, 'B');
